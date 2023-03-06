e9b0006a43f1d1b9fc202b131bd145f9ab6fbe92
hosanna57 <123368501+hosanna57@users.noreply.github.com>
2023-03-06 03:29:35 +0000

    practice

--- c-programs/01_demo.c
+++ c-programs/01_demo.c
@@ -3,10 +3,9 @@
 int main(){
     int i = 2, j = 3;
 	int *p = &i;
-
-    printf("value of i = %d\n", *p);
 	int *pj = &j;
 
+    printf("value of i = %d\n", *p);
     printf("value of j = %d\n", *pj);
 	printf("address of i = %p\n", &i);
 	printf("address of j = %p\n", &j);
